49670cde60d555ccfe637a3b738ca6390bd6e1ef
Anthony Willinthon Carriedo Peña <anthonycarriedo2013@gmail.com>
2021-07-02 12:01:59 -0400

    cambios v4

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        if($this->app->environment('local')) {
+        if(\App::environment('local')) {
             factory(User::class, 5)->create();
         }
         
